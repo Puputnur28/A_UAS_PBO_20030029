bf44662d3d0796089535dbb9bc8acac5ab390e7f
Puputnur28 <97465805+Puputnur28@users.noreply.github.com>
2022-01-27 10:12:29 +0800

    Add files via upload

---
+++ PBO_20030029.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int s,luas;
+
+    cout<<"masukan sisi luas bujursangkar : ";
+    cin>>s;
+
+    luas=s*s;
+    cout<<"luas bujursangkar adalah "<<luas;
+    return 0;
+}
